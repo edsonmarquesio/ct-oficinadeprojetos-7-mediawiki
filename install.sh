83709890958190d88e3a513bec6d8635ce6dd0d0
Edson Marques <edson.teixeira@bs2.com>
2022-12-01 00:27:12 -0300

    Install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,20 @@
 sudo apt-get update -y &&
 sudo apt-get install php php-apcu php-intl php-mbstring php-xml php-mysql php-calendar mariadb-server apache2 -y
 
-### Download dos Binários e Instalaca MediaWiki
+### Download dos Binários
 
 sudo chmod 777 /var/www
 sudo wget -c https://releases.wikimedia.org/mediawiki/1.38/mediawiki-1.38.4.tar.gz &&
 sudo tar -xf mediawiki-*.tar.gz -C /var/www/html &&
 sudo mv /var/www/html/mediawiki-1.38.4 /var/www/html/w &&
-sudo rm -f mediawiki-*.tar.gz
+sudo mv /var/www/html/w/* /var/www/html &&
+sudo rm -f mediawiki-*.tar.gz &&
+sudo rm -f /var/www/html/index.html
+
+### Instalacao MediaWiki
+
+cd /var/www/html
+sudo php maintenance/install.php --dbname=my_wiki --dbserver="dbserver:3306" --installdbuser=admin --installdbpass=password --dbuser=admin --dbpass=password --server="http://localhost" --scriptpath="" --lang=en --pass=password "MediaWiki" "admin"
 
 ### Start do Apache
 
