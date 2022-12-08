1fb2967092db13694ba198497b1dc699c1e6273c
Edson Marques <edson.teixeira@bs2.com>
2022-12-08 00:50:09 -0300

    Update Install.sh

--- install.sh
+++ install.sh
@@ -14,11 +14,19 @@ sudo mv /var/www/html/mediawiki-1.38.4 /var/www/html/w &&
 sudo mv /var/www/html/w/* /var/www/html &&
 sudo rm -f mediawiki-*.tar.gz &&
 sudo rm -f /var/www/html/index.html
+sudo git clone --depth 1 https://github.com/edwardspec/mediawiki-aws-s3.git AWS
+sudo mv ./AWS /var/www/html/extensions
+sudo wget -cO - https://getcomposer.org/composer-2.phar > composer.phar
+sudo mv composer.phar composer
+sudo chmod +x composer
+sudo composer update
 
 ### Instalacao MediaWiki
 
 cd /var/www/html
 sudo php maintenance/install.php --dbname=my_wiki --dbserver="dbserver:3306" --installdbuser=admin --installdbpass=password --dbuser=admin --dbpass=password --server="http://localhost" --scriptpath="" --lang=en --pass=password "MediaWiki" "admin"
+sudo echo '$wgSessionCacheType = CACHE_DB;' >> /var/www/html/LocalSettings.php
+sudo echo '$wgFileBackends['s3']['privateWiki'] = true;' >> /var/www/html/LocalSettings.php
 
 ### Start do Apache
 
